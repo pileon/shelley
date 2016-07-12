59ef7528ea9f8c657b942ecf4eaae1592271f94a
Joachim Pileborg <arrow@pileborg.se>
2016-07-12 15:48:29 +0200

    Added include subdirectory

---
+++ include/shelley/shelley.h
@@ -0,0 +1,5 @@
+#pragma once
+#ifndef SHELLEY_SHELLEY_H
+#define SHELLEY_SHELLEY_H
+
+#endif //SHELLEY_SHELLEY_H
--- src/shelley.cpp
+++ src/shelley.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include <shelley/shelley.h>
+
 int main()
 {
     std::cout << "Shelley says \"Hello\"\n";
