2bc5f7255ddd23ff03aba7664c95edc64f6c37b2
Joachim Pileborg <joachim.pileborg@lexiconitkonsult.se>
2016-08-04 13:58:07 +0200

    Reading input

--- src/shelley.cpp
+++ src/shelley.cpp
@@ -7,5 +7,16 @@ using namespace shelley;
 
 int main()
 {
-    std::cout << utility::get_prompt();
+    std::cout << utility::get_prompt() << std::flush;
+
+    std::string input;
+    if (!std::getline(std::cin, input))
+    {
+        if (!std::cin.eof())
+            std::cerr << "\nError reading input\n";
+
+        return 0;
+    }
+
+    std::cout << "Input was \"" << input << "\"\n";
 }
