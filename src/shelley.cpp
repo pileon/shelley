bdd1ca71095faca426893ca582716ddcc18a6bc3
Joachim Pileborg <joachim.pileborg@lexiconitkonsult.se>
2016-08-04 14:32:39 +0200

    Code to clean up the input, and split it on white-space

--- src/shelley.cpp
+++ src/shelley.cpp
@@ -1,7 +1,12 @@
-#include <iostream>
-
 #include <shelley/shelley.h>
 #include <shelley/utility.h>
+
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+#include <vector>
+#include <iterator>
+#include <sstream>
 
 using namespace shelley;
 
@@ -18,5 +23,30 @@ int main()
         return 0;
     }
 
-    std::cout << "Input was \"" << input << "\"\n";
+    std::cout << "1: Input is \"" << input << "\"\n";
+
+    // Turn multiple white-space into single white-space
+    input.erase(std::unique(std::begin(input), std::end(input), [](auto const& first, auto const& second)
+    {
+        return std::isspace(first) && std::isspace(second);
+    }), std::end(input));
+
+    std::cout << "2: Input is \"" << input << "\"\n";
+
+    // Turn all white-space into a space
+    std::replace_if(std::begin(input), std::end(input), [](auto const& ch)
+    {
+        return std::isspace(ch);
+    }, ' ');
+
+    std::cout << "3: Input is \"" << input << "\"\n";
+
+    // Split each "word" into a vector
+    std::istringstream input_stream{input};
+    std::vector<std::string> command{std::istream_iterator<std::string>{input_stream},
+                                     std::istream_iterator<std::string>{}};
+
+    std::cout << "4: command is\n";
+    for (auto const& s : command)
+        std::cout << "    \"" << s << "\"\n";
 }
