7af5d83abba7522012a657deb904056180b8440c
Joachim Pileborg <joachim.pileborg@lexiconitkonsult.se>
2016-08-11 14:06:12 +0200

    Added function to convert a vector of string to an argv-style vector

--- include/shelley/utility.h
+++ include/shelley/utility.h
@@ -31,6 +31,18 @@ namespace shelley
          * @return A vector of strings
          */
         std::vector<std::string> tokenize(std::string input);
+
+        /**
+         * \brief Convert a vector of string objects to a vector of pointers to characters
+         *
+         * To execute a command we need an argv-style array. This function creates such
+         * an array in the form of a vector. This function also adds an extra null-pointer
+         * element at the end.
+         *
+         * @param arguments the vector to convert
+         * @return a vector of pointers to characters, terminated by a null pointer
+         */
+        std::vector<char*> arguments_to_argv(std::vector<std::string> const& arguments);
     }
 }
 
