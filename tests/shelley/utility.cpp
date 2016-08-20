b876df00588e86b8237e68769e3cdc2a8beda4be
Joachim Pileborg <arrow@pileborg.se>
2016-08-21 00:12:41 +0200

    Added tests for tokenization

--- tests/shelley/utility.cpp
+++ tests/shelley/utility.cpp
@@ -1,12 +1,21 @@
 #include <gtest/gtest.h>
 #include <shelley/utility.h>
 
-TEST(dummy, dummy)
-{
-    EXPECT_EQ(1, 1);
-}
-
 TEST(utility, get_prompt)
 {
     EXPECT_EQ(shelley::utility::get_prompt(), "# ");
+}
+
+TEST(utility, tokenize)
+{
+    auto tokens = shelley::utility::tokenize("foo bar baz");
+
+    EXPECT_EQ(tokens.size(), 3ul);
+
+    if (tokens.size() >= 3)
+    {
+        EXPECT_EQ(tokens[0], "foo");
+        EXPECT_EQ(tokens[1], "bar");
+        EXPECT_EQ(tokens[2], "baz");
+    }
 }
